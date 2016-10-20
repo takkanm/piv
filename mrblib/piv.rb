3ad5d05d1463262d7e01d3062bd4cc655a52b0e6
takkanm <takkanm@gmail.com>
2016-10-20 19:33:11 +0900

    get /me

--- mrblib/piv.rb
+++ mrblib/piv.rb
@@ -11,12 +11,37 @@ class PivConfig
   end
 end
 
+class PivotalTrackerApiClient
+  API_HOST = 'www.pivotaltracker.com'
+
+  def initialize(project_id, token)
+    @token = token
+    @project_id = project_id
+  end
+
+  def me
+    get('/services/v5/me')
+  end
+
+  def get(path, header = {})
+    client.request('GET', path, header.merge(default_header)).body
+  end
+
+  def default_header
+    {'X-TrackerToken' => @token}
+  end
+
+  def client
+    @client ||= SimpleHttp.new('https', API_HOST)
+  end
+end
+
 def __main__(argv)
   if argv[1] == "version"
     puts "v#{Piv::VERSION}"
   else
     config = PivConfig.new
-    p config
-    p SimpleHttp.new("https", config['url']).request("GET", "/", {'User-Agent' => "test-agent"})
+    client = PivotalTrackerApiClient.new(config['project_id'], config['token'])
+    p client.me
   end
 end
